78571b3c532f6774d90955d16a0127263bb3181a
AlejandroMartinez02 <martinezmorilloalejandro@gmail.com>
2022-10-18 14:20:05 +0200

    textTextField

---
+++ textTextField/app/src/main/java/com/dam2/MainActivity.kt
@@ -0,0 +1,179 @@
+package com.dam2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.ResourceFont
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.res.ResourcesCompat
+import com.dam2.ui.theme.TextTextFieldTheme
+import com.dam2.ui.theme.anton
+import com.dam2.ui.theme.silk
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TextTextFieldTheme {
+               Column {
+                   Text1()
+                   Text2()
+                   Text3()
+                   Text4()
+                   Text5()
+                   Text6()
+                   TextField1()
+                   TextField2()
+               }
+            }
+        }
+    }
+
+    @Composable
+    @Preview
+    fun Text1(){
+            Text(text = "Este es el texto 1",
+                color = Color.Red,
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                fontFamily = FontFamily.Monospace,
+                letterSpacing = 4.sp
+        )
+    }
+    @Composable
+    @Preview
+    fun Text2(){
+        Text(text = "Este texto está tachado y rosa",
+            textDecoration = TextDecoration.LineThrough,
+            color = Color.Magenta,
+            fontSize = 20.sp,
+            lineHeight = 10.sp,
+            fontFamily = anton
+        )
+    }
+
+    @Composable
+    @Preview
+    fun Text3(){
+        val offset = Offset(10.0f, 4.0f)
+        Text(text = "Hola! Tengo sombra :0",
+            style = TextStyle(
+                fontSize = 24.sp,
+                shadow = Shadow(
+                    color = Color.Black,
+                    offset = offset,
+                    blurRadius = 3f
+                ),
+                color = Color.White,
+            )
+        )
+    }
+
+    @Composable
+    @Preview
+    fun Text4(){
+        val offset = Offset(5.0f, 4.0f)
+        Text(text = "¡Ahora parezco un cartel luminoso!",
+        style = TextStyle(
+            shadow = Shadow(
+                color = Color.Magenta,
+                offset = offset,
+                blurRadius = 20F
+            ),
+            fontFamily = silk,
+            color = Color.Yellow,
+            fontSize = 25.sp
+        ))
+    }
+
+    @Composable
+    @Preview
+    fun Text5(){
+        val offset = Offset(5.0f, 4.0f)
+        Text(buildAnnotatedString {
+            withStyle(style = SpanStyle(color = Color.Blue, fontSize = 10.sp)){
+                append("es")
+            }
+            append("te texto es")
+            withStyle(style = SpanStyle(shadow = Shadow(
+                color = Color.Blue,
+                offset = offset,
+                blurRadius = 20F
+            ), color = Color.Red)
+            ){
+                append(" algo ra")
+            }
+            append("ro")
+        })
+    }
+
+    @Composable
+    @Preview
+    fun Text6(){
+        Text(text = "Este es el último texto",
+            color = Color.Green,
+            fontFamily = anton,
+            textDecoration = TextDecoration.Underline,
+            fontSize = 15.sp,
+            letterSpacing = 10.sp
+        )
+
+    }
+
+    @Composable
+    @Preview
+    fun TextField1(){
+        var text by remember{ mutableStateOf("") }
+            TextField(value = text,
+            onValueChange = { text = it },
+            label = {Text("Primer TextField")},
+            textStyle = TextStyle(fontFamily = anton),
+            maxLines = 1,
+            modifier = Modifier.border(BorderStroke(5.dp, Color.Blue))
+
+        )
+    }
+
+    @Composable
+    @Preview
+    fun TextField2(){
+
+        TextField(value = "¡En este no puedes escribir!",
+            onValueChange = { it },
+            label = {Text("Deshabilitado", fontFamily = silk)},
+            enabled = false,
+            textStyle = TextStyle(color = Color.Magenta, fontFamily = silk),
+            modifier = Modifier.border(BorderStroke(5.dp, Color.Red))
+        )
+    }
+
+}
+
+
---
+++ textTextField/app/src/main/java/com/dam2/ui/theme/Type.kt
@@ -0,0 +1,36 @@
+package com.dam2.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.dam2.R
+
+val anton = FontFamily(
+    Font(R.font.antonregular)
+)
+
+val silk = FontFamily(
+    Font(R.font.silkscreenregular)
+)
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
