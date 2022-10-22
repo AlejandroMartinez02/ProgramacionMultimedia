c1e9cbcaa04b2fabb16defd43153ca5163951220
AlejandroMartinez02 <martinezmorilloalejandro@gmail.com>
2022-10-22 13:49:31 +0200

    Actualizar el ProgressBar

--- ProgressBar/app/src/main/java/com/alejandro/progressbar/MainActivity.kt
+++ ProgressBar/app/src/main/java/com/alejandro/progressbar/MainActivity.kt
@@ -3,13 +3,15 @@ package com.alejandro.progressbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alejandro.progressbar.ui.theme.ProgressBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ProgressBarTheme {
+                MyProgressBar()
             }
         }
     }
@@ -26,6 +29,62 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun ProgressBar(){
+fun MyProgressBar(){
+    var showloading by rememberSaveable{
+        mutableStateOf(false)
+    }
+    var processStatus by rememberSaveable {
+        mutableStateOf(0F)
+    }
 
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        if(showloading) {
+            CircularProgressIndicator(
+                color = Color.Red,
+                strokeWidth = 3.dp,
+                progress = processStatus
+            )
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 32.dp),
+                color = Color.Green,
+                backgroundColor = Color.LightGray,
+                progress = processStatus
+            )
+            Row(
+                Modifier.padding(top = 8.dp),
+                Arrangement.SpaceAround,
+                Alignment.CenterVertically
+            ){
+                Button(
+                    onClick ={
+                        if(processStatus > 0f && (processStatus-0.1f) > 0f)
+                            processStatus -= 0.1f
+                    }) {
+                    Text(text = "Reducir")
+                }
+                Button(
+                    onClick = {
+                        if(processStatus < 1f)
+                            processStatus += 0.1f
+                    }
+                ) {
+                    Text(text = "Incrementar")
+                }
+            }
+        }
+
+
+        Button(
+            modifier = Modifier.padding(top = 100.dp),
+            onClick = { showloading = !showloading }
+        ) {
+            Text(text = "Activar / Desactivar")
+        }
+
+    }
 }
